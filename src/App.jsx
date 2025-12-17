2f3681bfe09b73a4a0c92fe5f5b895c71ee479c2
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-17 17:20:19 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from 'react';
+import Home from './pages/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
-    <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
