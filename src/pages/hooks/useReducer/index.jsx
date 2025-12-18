975ac324af0be3891bf2b6be3794ee69a247d3f9
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-18 15:45:31 +0500

    .

--- src/pages/hooks/useReducer/index.jsx
+++ src/pages/hooks/useReducer/index.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import BackToHomeBtn from '../../../components/backToHomeBtn'
 
 const UseReducer = () => {
+  function UseReducer(count, setCount) {
+    switch (setCount.type) {
+      case "increment":
+        return count + 1
+      case "decrement":
+        return count - 1
+      default:
+        return count
+    }
+  }
+  const [count, setCount] = React.useReducer(UseReducer, 0)
   return (
     <>
       <BackToHomeBtn />
       <h2>This is the useReducer Page</h2>
+      <button onClick={() => setCount({type: "increment"})}>+</button>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount({type:"decrement"})}>-</button>
     </>
   )
 }
