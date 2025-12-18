208cf590568536a82382d7b22c2f26ca23c4aca7
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-18 12:40:06 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/home';
+import UseState from './pages/useState';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/useState" element={<UseState />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const Navigate = useNavigate();
   return (
     <>
         <h2>Welcome to the Home Page</h2>
-        
+        <button onClick={() => Navigate('/useState')}>useState</button>
     </>
   )
 }
