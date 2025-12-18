d6c09ea6b5cf266b1c9e0a13b94011ce33b7eb69
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-18 23:54:16 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Home from './pages/home';
 import UseState from './pages/hooks/useState';
 import UseEffect from './pages/hooks/useEffect';
 import UseReducer from './pages/hooks/useReducer';
 import UseRef from './pages/hooks/useRef';
+import UseMemo from './pages/hooks/useMemo';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/useState" element={<UseState />} />
           <Route path='/useEffect' element={<UseEffect />} />
           <Route path='/useReducer' element={<UseReducer />} />
+          <Route path='/useMemo' element={<UseMemo />} />
           <Route path='/useRef' element={<UseRef />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/hooks/useMemo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BackToHomeBtn from '../../../components/backToHomeBtn'
+
+const UseMemo = () => {
+    return (
+        <>
+            <BackToHomeBtn />
+            <h2>This is the useMemo Page</h2>
+        </>
+    )
+}
+
+export default UseMemo
