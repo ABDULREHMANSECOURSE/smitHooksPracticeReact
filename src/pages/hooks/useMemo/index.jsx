11d378106e3b016871d92fc41e2854a53a614243
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-19 07:20:16 +0500

    .

--- src/pages/hooks/useMemo/index.jsx
+++ src/pages/hooks/useMemo/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import BackToHomeBtn from '../../../components/backToHomeBtn'
 
 const UseMemo = () => {
+    const [count, setCount] = useState(0)
+
+    const title = useMemo( () => {
+        return "useMemo - Count: " + count ;
+    }, [count])
+    document.title = title;
     return (
         <>
             <BackToHomeBtn />
             <h2>This is the useMemo Page</h2>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <h3>{count}</h3>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
         </>
     )
 }
