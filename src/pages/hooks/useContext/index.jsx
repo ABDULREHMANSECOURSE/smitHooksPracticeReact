f5e9a8b275bef8b6672f568c6322f44b4def092c
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-21 15:45:55 +0500

    .

--- src/pages/hooks/useContext/index.jsx
+++ src/pages/hooks/useContext/index.jsx
@@ -3,7 +3,6 @@ import { CounterContext } from "../../../components/context/CounterContext";
 
 const UseContext = () => {
   const { count, setCount } = React.useContext(CounterContext);
-
   return (
     <>
       <h3>Count: {count}</h3>
