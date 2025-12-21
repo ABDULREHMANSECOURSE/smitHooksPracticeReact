9e985df5d7180fd294d987decafd09192e1ead00
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-21 16:44:13 +0500

    .

--- src/pages/hooks/useContext/index.jsx
+++ src/pages/hooks/useContext/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { CounterContext } from "../../../components/context/CounterContext";
+import BackToHomeBtn from "../../../components/backToHomeBtn";
 
 const UseContext = () => {
   const { count, setCount } = React.useContext(CounterContext);
   return (
     <>
+      <BackToHomeBtn />
+      <h2>This is the useEffect Page</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
       <h3>Count: {count}</h3>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </>
   );
 };
