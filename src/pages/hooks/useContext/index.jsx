7691df9c4265c394b2e3c808ebb3e0930f28d6fd
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-21 12:36:10 +0500

    .

--- src/pages/hooks/useContext/index.jsx
+++ src/pages/hooks/useContext/index.jsx
@@ -1,3 +1,30 @@
-import { createContext } from "react";
-const CounterContext = createContext()
+import React, { createContext, useState } from "react";
 
+// 1️⃣ Context create
+const CounterContext = createContext();
+
+const UseContext = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    // 2️⃣ Provider SAME PAGE
+    <CounterContext.Provider value={{ count, setCount }}>
+      <CounterUI />
+    </CounterContext.Provider>
+  );
+};
+
+// 3️⃣ useContext Provider ke ANDAR
+const CounterUI = () => {
+  const { count, setCount } = React.useContext(CounterContext);
+
+  return (
+    <>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </>
+  );
+};
+
+export default UseContext;
